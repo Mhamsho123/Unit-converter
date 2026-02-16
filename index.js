b4b605957b3c6fca17fa87918e04e0553718e71d
Mohamad Hamsho <hamsho1030@gmail.com>
2026-02-16 11:58:45 -0500

    Added functionality to button in order to render solutions and also added some css

---
+++ index.js
@@ -0,0 +1,37 @@
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input-element")
+let lengthEl = document.getElementById("length-element")
+let volumeEl = document.getElementById("volume-element")
+let massEl = document.getElementById("mass-element")
+
+
+
+convertBtn.addEventListener("click", function () {
+    renderLength(inputEl.value)
+    renderVolume(inputEl.value)
+    renderMass(inputEl.value)
+    inputEl.value = ""
+
+})
+
+
+function renderLength(value) {
+
+    let convertedFeet = value * 3.281
+    let convertedMeters = value / 3.281
+    lengthEl.textContent = `${value} meters = ${convertedFeet.toFixed(3)} feet | ${value} feet = ${convertedMeters.toFixed(3)} meters `
+}
+
+function renderVolume(value) {
+
+    let convertedGallon = value * 0.264
+    let convertedLiter = value / 0.264
+    volumeEl.textContent = `${value} liters = ${convertedGallon.toFixed(3)} gallons | ${value} gallons = ${convertedLiter.toFixed(3)} liters `
+}
+
+function renderMass(value) {
+
+    let convertedPounds = value * 2.204
+    let convertedKilos = value / 2.204
+    massEl.textContent = `${value} kilos = ${convertedPounds.toFixed(3)} pounds | ${value} pounds = ${convertedKilos.toFixed(3)} kilos `
+}
